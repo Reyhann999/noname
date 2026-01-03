e39ce24ac3baf0a636bc7c32f4b48f01e4f8a3a2
reyhan <reyhanalfarisi89@gmail.com>
2026-01-03 22:30:44 +0700

    make cart more interactive

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// menu dan harganya
 const menu = {
   "Martabak Coklat":20000,
   "Martabak Keju":22000,
   "Pancake":15000
 };
 
+// dibuat array
 const menuItem = Object.keys(menu);
 const menuPrice = Object.values(menu);
 
@@ -49,8 +51,72 @@ for (let i = 0; i < item.length; i++) {
 
 
 const cart = document.querySelector(".cart");
+const cartBody = document.querySelector(".cart-body");
+const total = document.querySelector(".total");
+
+let pTotal = 0;
+const cartMenu = document.querySelectorAll(".items");
 
 function sumPrice(el) {
   const id = Number(el.dataset.id);
-  cart.textContent = "halo, aku adalah " + menuItem[id];
+
+  // cegah dobel
+  if (cartBody.querySelector(`[data-id="${id}"]`)) return;
+  
+  areAllCheckboxChecked("cart-item");
+  cartBody.insertAdjacentHTML(
+    'beforeend',
+    `
+    <label>
+      <input class="cart-item" type="checkbox" data-id="${id}" checked>
+      ${menuItem[id]}
+    </label>
+    `
+  );
+  cartBody.addEventListener('change', e => {
+  if (!e.target.classList.contains('cart-item')) return;
+  hitungUlangTotal();
+  areAllCheckboxChecked("cart-item");
+});
+  hitungUlangTotal();
+}
+
+function hitungUlangTotal() {
+  let totalBaru = 0;
+
+  document.querySelectorAll('.cart-item').forEach(item => {
+    if (item.checked) {
+      totalBaru += menuPrice[item.dataset.id];
+    }
+  });
+
+  pTotal = totalBaru;
+  total.textContent = pTotal;
+}
+
+function expandCart() {
+  const triangle = document.querySelector(".triangle-down");
+  triangle.classList.toggle("expanded");
+  if (triangle.classList.contains("expanded")) {
+    cartBody.style.display = "flex";
+  } else {
+    cartBody.style.display = "none"
+  }
+}
+
+function areAllCheckboxChecked(classname) {
+  const checkboxes = document.querySelectorAll(`input[type="checkbox"].${classname}`);
+  const allChecked = Array.from(checkboxes).every(checkbox => {
+    return checkbox.checked;
+  })
+  
+  const semuaItem = document.querySelector("#semua-item");
+  if (allChecked) {
+    semuaItem.checked = true;
+  } else {
+    semuaItem.checked = false;
+  }
+    
+    
+  return allChecked;
 }
